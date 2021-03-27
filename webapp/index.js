cf79e996a185853d46fe29c8c05e6b954cadd43a
MPiat <mpiatek010@gmail.com>
2021-03-27 10:35:38 +0100

    Add storing and retrieving data from redis cache

--- webapp/index.js
+++ webapp/index.js
@@ -2,26 +2,15 @@ const express = require("express")
 const cors = require('cors');
 
 const app = express()
-const redis = require('redis');
 const queries = require('./queries')
 
 
 app.use(cors());
 app.use(express.json());
 
-// Creating and testing redis connection
-const redisClient = redis.createClient({
-    host: "myredis",
-    port: 6379,
-    // retry_strategy: () => 1000
-});
 
-redisClient.on('connect', () => {
-    console.log('Connected to Redis server');
-});
 
 // Creating connection to postgresql and creating table if it doesn't exists
-
 queries.checkOrInstantiateTable();
 
 app.get('/hello',(req, res) => {
@@ -37,9 +26,8 @@ app.delete('/cats/:id', queries.deleteCat);
 
 
 
-
 const PORT = 9090;
 
 app.listen(PORT, () => {
-    console.log(`API is listening of port ${PORT}`);
+    console.log(`API is listening on port ${PORT}`);
 });
--- webapp/queries.js
+++ webapp/queries.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const redis = require('redis');
 
 const pgClient = new Pool({
     user: "postgres",
@@ -8,9 +9,21 @@ const pgClient = new Pool({
     port: "5432"
 });
 
-pgClient.on('error', () => {
-    console.log("Postgres not conencted");
+// pgClient.on('error', () => {
+//     console.log("Postgres not connected");
+// });
+
+
+// Creating and testing redis connection
+const redisClient = redis.createClient({
+  host: "myredis",
+  port: 6379,
+  // retry_strategy: () => 1000
 });
+
+// redisClient.on('connect', () => {
+//   console.log('Connected to Redis server');
+// });
 
 function checkOrInstantiateTable(){
     pgClient.query(`CREATE TABLE IF NOT EXISTS cats (
@@ -35,23 +48,51 @@ const getCats = (request, response) => {
 const getCatById = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pgClient.query('SELECT * FROM cats WHERE id = $1', [id], (error, results) => {
+  // Check if cat with given id exists in Redis and if it does fetch it from there
+  // if not then fetch it from postgres
+  redisClient.exists(id, (error, result) => {
     if (error) {
-      throw error
+      throw error;
     }
-    response.status(200).json(results.rows)
-  })
+    
+
+    if(result == 1) {
+      console.log("Found data in Redis cache");
+      redisClient.hgetall(id, function(error, object) {
+        if(error) {
+          throw error;
+        }
+        response.status(200).header('cache', 'true').json(object);
+      })
+    } else {
+      console.log("Data not found in Redis cache")
+      pgClient.query('SELECT * FROM cats WHERE id = $1', [id], (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).json(results.rows);
+      })
+    }
+  });
 };
 
 const createCat = (request, response) => {
     const { name, age } = request.body
-  
-    pgClient.query('INSERT INTO cats (name, age) VALUES ($1, $2)', [name, age], (error, result) => {
+    
+    pgClient.query('INSERT INTO cats (name, age) VALUES ($1, $2) RETURNING ID', [name, age], (error, result) => {
       if (error) {
-        throw error
+        throw error;
       }
-      response.status(201).send(`Cat added with ID: ${result.insertId}`)
+
+      var record_id = String(result.rows[0].id);
+
+      // Adding cat to Redis
+      console.log(typeof(record_id));
+      redisClient.hmset(record_id,'name', name, 'age', age);
+
+      response.status(201).send(`Cat added with ID: ${record_id}`)
     })
+    
 };
 
 const updateCat = (request, response) => {
@@ -63,7 +104,7 @@ const updateCat = (request, response) => {
     [name, age, id],
     (error, results) => {
       if (error) {
-        throw error
+        throw error;
       }
       response.status(200).send(`Cat modified with ID: ${id}`)
     }
